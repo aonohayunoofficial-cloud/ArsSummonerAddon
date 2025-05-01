3a830b84e9bebfbfee2132e6b79f19a66886c502
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2025-05-01 23:06:36 +0200

    Update to 1.21.1 - pre doc update (#7)
    * Update to 1.21.1 - pre doc update
    
    * remove deprecated book.json

--- src/main/java/com/example/an_addon/ExampleANAddon.java
+++ src/main/java/com/example/an_addon/ExampleANAddon.java
@@ -2,40 +2,37 @@ package com.example.an_addon;
 
 import com.example.an_addon.registry.ModRegistry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleANAddon.MODID)
-public class ExampleANAddon
-{
+public class ExampleANAddon {
     public static final String MODID = "an_addon";
 
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleANAddon() {
-        IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
-        ModRegistry.registerRegistries(modbus);
+    public ExampleANAddon(IEventBus modEventBus, ModContainer modContainer) {
+        ModRegistry.registerRegistries(modEventBus);
         ArsNouveauRegistry.registerGlyphs();
-        modbus.addListener(this::setup);
-        modbus.addListener(this::doClientStuff);
-        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::setup);
+        modEventBus.addListener(this::doClientStuff);
+        NeoForge.EVENT_BUS.register(this);
     }
 
-    public static ResourceLocation prefix(String path){
-        return new ResourceLocation(MODID, path);
+    public static ResourceLocation prefix(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MODID, path);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
         ArsNouveauRegistry.registerSounds();
     }
 
